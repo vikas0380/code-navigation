2c962924c6343113f537f32c64fcdce3ba01afc4
vikas <vikas@vikass-iMac.lan>
2016-08-09 22:26:43 +0530

    Extention Changes

---
+++ README.md
@@ -0,0 +1,11 @@
+# Code Navigation
+
+Code navigation makes Visual studio Code back and foward navigation pretty easy 
+
+## Installation
+
+Some people have reported that they cannot find the extension when they insert the installation command:
+```
+ext install code-navigation
+```
+
---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument}  from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext) {
+
+     let _statusBarItemBack: StatusBarItem;
+
+    let _statusBarItemFwd: StatusBarItem;
+
+if (!this._statusBarItemBack) {
+            this._statusBarItemBack = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+
+        if (!this._statusBarItemFwd) {
+            this._statusBarItemFwd = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+
+        this._statusBarItemBack.command="workbench.action.navigateBack";
+        this._statusBarItemFwd.command="workbench.action.navigateForward";
+        
+
+        this._statusBarItemBack.text="$(arrow-left)";
+
+        this._statusBarItemFwd.text="$(arrow-right)";
+        this._statusBarItemBack.show()
+
+        this._statusBarItemFwd.show()
+
+
+
+//let  nav = new Navigation()
+   console.log('Navigation is active!');
+
+    context.subscriptions.push(_statusBarItemFwd);
+    context.subscriptions.push(_statusBarItemBack);
+    
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+
+}
+
+
